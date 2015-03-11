2465486de6967d3930080fbea61ccc74238db490
Kiko Lobo <kixlobo@gmail.com>
2015-03-11 04:46:31 -0600

    Add: Login/Logout Functionality.

--- Example/spark-sdk/SparkViewController.m
+++ Example/spark-sdk/SparkViewController.m
@@ -7,17 +7,46 @@
 //
 
 #import "SparkViewController.h"
+#import "SparkLoginViewController.h"
+#import <SparkCloud.h>
 
 @interface SparkViewController ()
+@property (weak, nonatomic) SparkCloud *cloud;
+@property (weak, nonatomic) IBOutlet UIButton *logoutButton;
 
 @end
 
 @implementation SparkViewController
+//TODO: Make this a table view to display menu options to navigate to.
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.cloud = [SparkCloud sharedInstance];
+    self.logoutButton.enabled = NO;
+    self.title = @"No User";
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    if (!self.cloud.isUserLoggedIn) {
+        [SparkLoginViewController presentLoginViewControllerFromViewController:self
+                                                                withCompletion:^(SparkLoginViewController *loginViewController, SparkUser *user) {
+                                                                            [self dismissViewControllerAnimated:YES completion:nil];
+                                                                    NSLog(@"User: %@", user.user);
+                                                                }];
+    } else {
+        NSLog(@"User: %@ is logged in", self.cloud.loggedInUsername);
+        self.logoutButton.enabled = YES;
+        self.title = self.cloud.loggedInUsername;
+    }
+    
+}
+
+- (IBAction)logoutButtonPressed:(id)sender {
+    [self.cloud logout];
+    self.logoutButton.enabled = NO;
+    self.title = @"";
 }
 
 - (void)didReceiveMemoryWarning
