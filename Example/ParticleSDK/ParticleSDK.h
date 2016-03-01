78d493456372baf9c306c23a865918719e151ac5
Ido Kleinman <idokleinman@gmail.com>
2016-02-29 18:25:57 -0800

    Adding framework (dynamic lib) target for Spark-SDK

---
+++ Example/ParticleSDK/ParticleSDK.h
@@ -0,0 +1,19 @@
+//
+//  ParticleSDK.h
+//  ParticleSDK
+//
+//  Created by Ido Kleinman on 2/29/16.
+//  Copyright © 2016 Ido Kleinman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ParticleSDK.
+FOUNDATION_EXPORT double ParticleSDKVersionNumber;
+
+//! Project version string for ParticleSDK.
+FOUNDATION_EXPORT const unsigned char ParticleSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParticleSDK/PublicHeader.h>
+
+
